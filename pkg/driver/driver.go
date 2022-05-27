89739eaf883495b3f16668f567d27dfaa0d3463e
Jonathan Rainer <jonrainer@googlemail.com>
2022-05-27 21:34:45 +0100

    Adds function and tests to support spaces in tags

--- pkg/driver/driver.go
+++ pkg/driver/driver.go
@@ -18,6 +18,7 @@ package driver
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"strings"
 
@@ -140,10 +141,83 @@ func parseTagsFromStr(tagStr string) map[string]string {
 		klog.Infof("Did not find any input tags.")
 		return m
 	}
-	tagsSplit := strings.Split(tagStr, " ")
+	tagsSplit := extractPairsFromRawString(tagStr)
+
 	for _, pair := range tagsSplit {
-		p := strings.Split(pair, ":")
-		m[p[0]] = p[1]
+		k, v, err := extractKeyAndValueFromRawPair(pair)
+		if err != nil {
+			klog.Warningf("Could not extract key and value from %s - %v", pair, err)
+			continue
+		}
+		m[k] = v
 	}
 	return m
+}
+
+func extractPairsFromRawString(raw string) []string {
+	result := make([]string, 0)
+	accumulator := ""
+	chars := []rune(raw)
+	literal := false
+	for i := 0; i < len(chars); i++ {
+		switch chars[i] {
+		case '\'':
+			literal = !literal
+			accumulator += string(chars[i])
+		case ' ':
+			if literal {
+				accumulator += string(chars[i])
+			} else {
+				result = append(result, accumulator)
+				accumulator = ""
+			}
+		default:
+			accumulator += string(chars[i])
+		}
+	}
+	if accumulator != "" {
+		result = append(result, accumulator)
+	}
+	return result
+}
+
+func extractKeyAndValueFromRawPair(pair string) (string, string, error) {
+	chars := []rune(pair)
+	key := ""
+	literal := false
+	finalKeyIndex := -1
+	for i := 0; i < len(chars); i++ {
+		switch chars[i] {
+		case '\'':
+			literal = !literal
+		case ':':
+			if literal {
+				key += string(chars[i])
+			} else {
+				finalKeyIndex = i
+				break
+			}
+		default:
+			key += string(chars[i])
+		}
+		if finalKeyIndex >= 0 {
+			break
+		}
+	}
+
+	if literal {
+		return "", "", fmt.Errorf("unmatched quotes in tag string")
+	} else if key == "" {
+		return "", "", fmt.Errorf("cannot have empty key")
+	}
+
+	value := string(chars[finalKeyIndex+1:])
+	if len(value) > 0 && value[0] == '\'' {
+		value = value[1:]
+	}
+	if len(value) > 0 && value[len(value)-1] == '\'' {
+		value = value[:len(value)-1]
+	}
+
+	return key, value, nil
 }
---
+++ pkg/driver/driver_test.go
@@ -0,0 +1,102 @@
+package driver
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func TestParseTags(t *testing.T) {
+	testCases := []struct {
+		name     string
+		testFunc func(t *testing.T)
+	}{{
+		name: "One Entry - No Spaces or Odd Characters",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "foo:bar", map[string]string{"foo": "bar"})
+		},
+	}, {
+		name: "Multiple Entries - No Spaces or Odd Characters",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "foo:bar bash:baz flip:flop", map[string]string{"foo": "bar", "bash": "baz", "flip": "flop"})
+		},
+	}, {
+		name: "Containing +, -, =, ., _, /",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "foo+bar-=.:bash_baz/", map[string]string{"foo+bar-=.": "bash_baz/"})
+		},
+	}, {
+		name: "Key contains single :",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "'foo:bar':bash", map[string]string{"foo:bar": "bash"})
+		},
+	}, {
+		name: "Key contains multiple :'s",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "'foo:bar:i:am:fish':bash", map[string]string{"foo:bar:i:am:fish": "bash"})
+		},
+	}, {
+		name: "Value contains multiple :'s",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "foo:bash:bar:i:am:fish", map[string]string{"foo": "bash:bar:i:am:fish"})
+		},
+	}, {
+		name: "Value behaves with quotes",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "foo:'bashbariamfish'", map[string]string{"foo": "bashbariamfish"})
+		},
+	}, {
+		name: "Key with spaces",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "'foo and another thing':bar", map[string]string{"foo and another thing": "bar"})
+		},
+	}, {
+		name: "Value with spaces",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "foo:'and another thing'", map[string]string{"foo": "and another thing"})
+		},
+	}, {
+		name: "Case Sensitivity Is Retained",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "Foo:'and anOther ThiNg'", map[string]string{"Foo": "and anOther ThiNg"})
+		},
+	}, {
+		name: "Unmatched Quotes are rejected",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "'Foo:bar", map[string]string{})
+		},
+	}, {
+		name: "Check the empty key is rejected",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, ":bar", map[string]string{})
+		},
+	}, {
+		name: "Check the empty key is rejected when quoted",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "'':bar", map[string]string{})
+		},
+	}, {
+		name: "Check the empty value is registered properly",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "Foo: Bar:Bash", map[string]string{"Foo": "", "Bar": "Bash"})
+		},
+	}, {
+		name: "Check the empty value is registered properly when quoted",
+		testFunc: func(t *testing.T) {
+			compareParseTags(t, "Foo:'' Bar:Bash", map[string]string{"Foo": "", "Bar": "Bash"})
+		},
+	}}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, tc.testFunc)
+	}
+}
+
+func compareParseTags(testCase *testing.T, input string, wanted map[string]string) {
+	got := parseTagsFromStr(input)
+	if !reflect.DeepEqual(got, wanted) {
+		wanted, _ := json.Marshal(wanted)
+		got, _ := json.Marshal(got)
+		testCase.Fatalf("Wanted %s, but got %s", wanted, got)
+	}
+}
