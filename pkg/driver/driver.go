f2426f5a1cf9ecb96fd470d912e5c3896ea96a7d
David Xu <hengweix@amazon.com>
2025-09-16 16:15:49 +0000

    Add concurrency control related args
    1. maxInflightMountCalls with OptIn arg
    In order to use maxInflightMountCalls, user has to set
    maxInflightMountCallsOptIn to true and set a positive value for
    maxInflightMountCalls.
    
    2. volumeAttachLimit with OptIn arg
    To use volumeAttachLimit, user has to set volumeAttachLimitOptIn to be
    true and set volumeAttachLimit to a positive value.

--- pkg/driver/driver.go
+++ pkg/driver/driver.go
@@ -33,7 +33,9 @@ const (
 	driverName = "efs.csi.aws.com"
 
 	// AgentNotReadyTaintKey contains the key of taints to be removed on driver startup
-	AgentNotReadyNodeTaintKey = "efs.csi.aws.com/agent-not-ready"
+	AgentNotReadyNodeTaintKey   = "efs.csi.aws.com/agent-not-ready"
+	UnsetMaxInflightMountCounts = -1
+	UnsetVolumeAttachLimit      = -1
 )
 
 type Driver struct {
@@ -53,9 +55,11 @@ type Driver struct {
 	adaptiveRetryMode        bool
 	tags                     map[string]string
 	lockManager              LockManagerMap
+	inFlightMountTracker     *InFlightMountTracker
+	volumeAttachLimit        int64
 }
 
-func NewDriver(endpoint, efsUtilsCfgPath, efsUtilsStaticFilesPath, tags string, volMetricsOptIn bool, volMetricsRefreshPeriod float64, volMetricsFsRateLimit int, deleteAccessPointRootDir bool, adaptiveRetryMode bool) *Driver {
+func NewDriver(endpoint, efsUtilsCfgPath, efsUtilsStaticFilesPath, tags string, volMetricsOptIn bool, volMetricsRefreshPeriod float64, volMetricsFsRateLimit int, deleteAccessPointRootDir bool, adaptiveRetryMode bool, maxInflightMountCallsOptIn bool, maxInflightMountCalls int64, volumeAttachLimitOptIn bool, volumeAttachLimit int64) *Driver {
 	cloud, err := cloud.NewCloud(adaptiveRetryMode)
 	if err != nil {
 		klog.Fatalln(err)
@@ -79,6 +83,8 @@ func NewDriver(endpoint, efsUtilsCfgPath, efsUtilsStaticFilesPath, tags string, 
 		adaptiveRetryMode:        adaptiveRetryMode,
 		tags:                     parseTagsFromStr(strings.TrimSpace(tags)),
 		lockManager:              NewLockManagerMap(),
+		inFlightMountTracker:     NewInFlightMountTracker(getMaxInflightMountCalls(maxInflightMountCallsOptIn, maxInflightMountCalls)),
+		volumeAttachLimit:        getVolumeAttachLimit(volumeAttachLimitOptIn, volumeAttachLimit),
 	}
 }
 
